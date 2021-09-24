4427df03e9c35f91648684883cd9781a8ace2201
Aditya Uphade <aditya.uphade@morningstar.com>
2021-09-24 12:02:07 +0530

    [Aditya] - Fixed MediaQueries for Footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="home">
-    <Header />    <!-- USING TEMPLATE OF HEADER COMPONENT -->
-    <video class="main-bg" autoplay muted loop>
-      <source src="https://assets.mixkit.co/videos/preview/mixkit-top-aerial-shot-of-seashore-with-rocks-1090-large.mp4" type="video/mp4"/>
-    </video>
+    <div class="home-header">
+      <Header />
+      <video class="main-bg" autoplay muted loop>
+        <source src="https://assets.mixkit.co/videos/preview/mixkit-top-aerial-shot-of-seashore-with-rocks-1090-large.mp4" type="video/mp4"/>
+      </video>
+    </div>
+        <!-- USING TEMPLATE OF HEADER COMPONENT -->
+    <div class="home content"></div>
+    <div class="home-footer"><Footer /></div>
   </div>  
 </template>
 
@@ -11,16 +16,29 @@
 import { Options, Vue } from 'vue-class-component';
 // @ is an alias to /src
 import Header from '@/components/Header/Header.vue'    //IMPORTING COMPONENTS
+import Footer from '@/components/Footer/Footer.vue'    //IMPORTING COMPONENTS
 
 @Options({
   components: {     //REGISTER COMPONENTS HERE
-    Header
+    Header,
+    Footer
   },
 })
 export default class Home extends Vue {}
 </script>
 
 <style scoped lang="scss">
+.main-bg{
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: -1;
+}
 // *{
 //   background: crimson;
 // }
