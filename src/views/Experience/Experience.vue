abdd2abe34733d338982ab3dbda323d1c0dd2d35
Aditya Uphade <aditya.uphade@morningstar.com>
2021-09-27 18:45:25 +0530

    [Aditya] - Section 1 of Experience page

--- src/views/Experience/Experience.vue
+++ src/views/Experience/Experience.vue
@@ -1,13 +1,39 @@
 <template>
-<div class="experience-header"><Header/></div>
-<div class="experience-content">
-  <div class="container-1"></div>
-  <!-- <div class="container-2"></div>
-  <div class="container-3"></div>
-  <div class="container-4"></div> -->
+<div class="home bg-white">
+  <div class="experience-header"><Header/></div>
+  <video class="experience-bg" autoplay muted loop>
+    <source src="https://dxaurk9yhilm4.cloudfront.net/experiences_hero.mp4?mtime=20190609072805">
+  </video>
+  <div class="experience-content">
+    <div class="container-1">
+      <div class="headerContent">
+      Memorable days. Exceptional nights. We believe it’s our mission to make every minute matter. So you can live your life to the fullest, with the people closest to you.
+      </div>
+      <div class="bodyContent">
+        <div class="leftBody">
+          <h1>Exclusive to<br>your world</h1>
+          Our worlds are designed to offer you endless <br>
+          opportunities to share incredible once-in-a-lifetime <br>
+          experiences and adventures every single minute of the <br>
+          day. Each of our settings offers a relaxed sense of <br>
+          community through exceptional clubhouses, <br>
+          world-class golf courses, welcoming staff and an <br>
+          extensive Outdoor Pursuits program. Create moments <br>
+          of joy and wonder, unforgettable memories and <br>
+          cherished times, together with those who matter most.
+        </div>
+        <div class="rightBody">
+          <img src="https://dxaurk9yhilm4.cloudfront.net/images/899/Scuba_5ae402c9837860cd6e4f65e304b3a5fa.jpg" alt="">
+        </div>
+      </div>
+    </div>
+    <!-- <div class="container-2"></div>
+    <div class="container-3"></div>
+    <div class="container-4"></div> -->
+  </div>
+  
+  <div class="experience-footer"><Footer/></div>
 </div>
-
-<div class="experience-footer"><Footer/></div>
 </template>
 
 <script type='Module' src="./Experience.ts"></script>
